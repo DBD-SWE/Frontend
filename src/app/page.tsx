035c9a291aab94b429b4923cc08f387c09f0140a
Jean-Paul <jean.paul.bassil@outlook.com>
2024-04-11 01:41:03 +0300

    feat: added the doughnut chart component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import SummaryCardsList from './components/ui/Dashboard/SummaryCardsList';
+import { Doughnut } from 'react-chartjs-2';
+import DoughnutChart from './components/ui/Dashboard/DoughnutChart';
 
+// make the label circle
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <SummaryCardsList />
+      {/* <SummaryCardsList /> */}
+      <DoughnutChart
+        labels={['Task', 'Blue', 'Yellow']}
+        stats={[300, 50, 100]}
+        colors={['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']}
+      />
     </div>
   );
 }
