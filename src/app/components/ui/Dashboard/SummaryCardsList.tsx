7340e947c629dae9509b76b33eaadb92707480a2
Jean-Paul <jean.paul.bassil@outlook.com>
2024-04-11 01:45:56 +0300

    fix: cleaned up the summaryCardList code

--- src/app/components/ui/Dashboard/SummaryCardsList.tsx
+++ src/app/components/ui/Dashboard/SummaryCardsList.tsx
@@ -4,50 +4,62 @@ import SummaryCard from './SummaryCard';
 type Props = {};
 
 const SummaryCardsList = (props: Props) => {
+  const data = [
+    {
+      title: 'Guest Houses',
+      amount: 130,
+      lastModified: 'Jan 8, 2024',
+      icon: 'icons/guestHouses.svg',
+      color: '#A7141C',
+    },
+    {
+      title: 'Guest Houses',
+      amount: 130,
+      lastModified: 'Jan 8, 2024',
+      icon: 'icons/guestHouses.svg',
+      color: '#A7141C',
+    },
+    {
+      title: 'Guest Houses',
+      amount: 130,
+      lastModified: 'Jan 8, 2024',
+      icon: 'icons/guestHouses.svg',
+      color: '#A7141C',
+    },
+    {
+      title: 'Guest Houses',
+      amount: 130,
+      lastModified: 'Jan 8, 2024',
+      icon: 'icons/guestHouses.svg',
+      color: '#A7141C',
+    },
+    {
+      title: 'Guest Houses',
+      amount: 130,
+      lastModified: 'Jan 8, 2024',
+      icon: 'icons/guestHouses.svg',
+      color: '#A7141C',
+    },
+    {
+      title: 'Guest Houses',
+      amount: 130,
+      lastModified: 'Jan 8, 2024',
+      icon: 'icons/guestHouses.svg',
+      color: '#A7141C',
+    },
+  ];
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-      <SummaryCard
-        title="Guest Houses"
-        amount={130}
-        lastModified="Jan 8, 2024"
-        icon="icons/guestHouses.svg"
-        color="#A7141C"
-      />
-      <SummaryCard
-        title="Guest Houses"
-        amount={130}
-        lastModified="Jan 8, 2024"
-        icon="icons/guestHouses.svg"
-        color="#A7141C"
-      />
-      <SummaryCard
-        title="Guest Houses"
-        amount={130}
-        lastModified="Jan 8, 2024"
-        icon="icons/guestHouses.svg"
-        color="#A7141C"
-      />
-      <SummaryCard
-        title="Guest Houses"
-        amount={130}
-        lastModified="Jan 8, 2024"
-        icon="icons/guestHouses.svg"
-        color="#A7141C"
-      />
-      <SummaryCard
-        title="Guest Houses"
-        amount={130}
-        lastModified="Jan 8, 2024"
-        icon="icons/guestHouses.svg"
-        color="#A7141C"
-      />
-      <SummaryCard
-        title="Guest Houses"
-        amount={130}
-        lastModified="Jan 8, 2024"
-        icon="icons/guestHouses.svg"
-        color="#A7141C"
-      />
+      {data.map((item, index) => (
+        <SummaryCard
+          key={index}
+          title={item.title}
+          amount={item.amount}
+          lastModified={item.lastModified}
+          icon={item.icon}
+          color={item.color}
+        />
+      ))}
     </div>
   );
 };
