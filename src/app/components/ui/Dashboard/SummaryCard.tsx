416bc3f2ed1882e97b7687e4d2ddae1a0d56424f
Jean-Paul <jean.paul.bassil@outlook.com>
2024-04-11 01:43:42 +0300

    feat: added the comments for summaryCard

--- src/app/components/ui/Dashboard/SummaryCard.tsx
+++ src/app/components/ui/Dashboard/SummaryCard.tsx
@@ -2,23 +2,27 @@ import Image from 'next/image';
 import React from 'react';
 
 type Props = {
-  title: string;
-  amount: number;
-  lastModified: string;
-  color: string;
-  icon: string;
+  title: string; // Title of the summary card
+  amount: number; // Amount to be displayed
+  lastModified: string; // Last modified date/time
+  color: string; // Color theme for certain elements in the card
+  icon: string; // Icon image source
 };
 
 const SummaryCard = ({ title, amount, lastModified, color, icon }: Props) => {
   return (
-    <div className="h-full flex flex-col items-center min-w-64">
-      <div className="w-full flex items-start justify-between rounded-xl space-x-24 md:space-x-0 px-6 py-10 shadow-xl">
+    <div className="flex h-full min-w-64 flex-col items-center">
+      <div className="flex w-full items-start justify-between space-x-24 rounded-xl px-6 py-10 shadow-xl md:space-x-0">
         <div>
-          <p className="mb-4 text-base sm:text-base font-bold">Total {title}</p>
-          <p className={`text-[${color}] mb-2 text-2xl sm:text-4xl font-extrabold`}>
+          <p className="mb-4 text-base font-bold sm:text-base">Total {title}</p>
+          <p
+            className={`text-[${color}] mb-2 text-2xl font-extrabold sm:text-4xl`}
+          >
             {amount}
           </p>
-          <p className="text-[.6rem] opacity-60">Last modified - {lastModified}</p>
+          <p className="text-[.6rem] opacity-60">
+            Last modified - {lastModified}
+          </p>
         </div>
         <Image src={icon} alt={title} width={30} height={30} />
       </div>
