7bdb2d058c1c7ddba8e17856e7df6386800ce6f9
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-13 12:55:54 +0300

    creating layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,42 @@
-import "./styles/globals.css"
-import { Inter as FontSans } from "next/font/google"
+import './styles/globals.css';
+import { Inter as FontSans } from 'next/font/google';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+// import {Input} from "@nextui-org/react";
 
-import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
         )}
       >
+        <header className="flex w-full flex-row items-center justify-between border-b-[1px] border-gray-100 px-7 py-3">
+          <Image
+            src="/assets/general/big-logo.png"
+            alt="Logo"
+            width={110}
+            height={110}
+          />
+          <div>
+
+          </div>
+        </header>
         {children}
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ import DoughnutSummaryCard from './components/ui/Dashboard/DoughnutSummaryCard';
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <DoughnutSummaryCard
+      {/* <DoughnutSummaryCard
         title="Services"
         total={216}
         lastModified="Jan 8, 2024"
         stats={[200, 40, 20]}
         labels={['Blat', 'Amchit', 'Mastita']}
         colors={['#ff0000', '#3d85c6', '#f4cccc']}
-      />
+      /> */}
     </div>
   );
 }
