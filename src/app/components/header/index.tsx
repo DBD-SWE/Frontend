cea64d432e32f88ae5421233111eb62ae0d4ba86
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-13 18:21:44 +0300

    make: logo image as link

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
-import SearchInput from "./search";
+import SearchInput from './search';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="flex w-full flex-row items-center justify-between border-b-[1px] border-gray-100 px-7 py-3">
-      <Image
-        src="/images/general/big-logo.png"
-        alt="Logo"
-        width={110}
-        height={110}
-      />
+      <Link href="/">
+        <Image
+          src="/images/general/big-logo.png"
+          alt="Logo"
+          width={110}
+          height={110}
+        />
+      </Link>
       <div className="flex flex-row items-center">
         <SearchInput />
       </div>
