2db4aeef0e29e2c310b1677fdf92dac8041b5e5a
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-15 17:29:31 +0300

    add: general head section

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,12 +1,16 @@
 import { PageTitle, SubTitle } from '@/components/text';
+import { Button } from '@nextui-org/react';
 
 export default function Users() {
   return (
-    <div className="flex flex-col">
+    <div className="flex w-full flex-col">
       {/* Head Section */}
-      <div>
-        <PageTitle>User Management</PageTitle>
-        <SubTitle>Users</SubTitle>
+      <div className="flex w-full flex-row items-start">
+        <div className="flex flex-col">
+          <PageTitle>User Management</PageTitle>
+          <SubTitle>Users</SubTitle>
+        </div>
+        {/* <Button>Download</Button> */}
       </div>
     </div>
   );
