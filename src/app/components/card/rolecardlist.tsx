2755af74063520e50e1821318a17dce2a4a56887
Peter Chahid <peterdaaboul@outlook.com>
2024-04-15 19:11:12 +0300

    created a rolecard list for the fixed cards that exist

---
+++ src/app/components/card/rolecardlist.tsx
@@ -0,0 +1,62 @@
+'use client';
+import dataentry from '../../../../public/images/RoleImages/dataentry.svg';
+import developer from '../../../../public/images/RoleImages/developer.svg';
+import support from '../../../../public/images/RoleImages/support.svg';
+import admin from '../../../../public/images/RoleImages/admin.svg';
+
+const roles = [
+    {
+      iconName: admin,
+      roleName: "Administrator",
+      userCount: 120,
+      listItems: [
+        "Manage Users",
+        "Configure Settings",
+        "Full Access Control",
+        "System Audits"
+      ],
+      borderColor: "border-t-red-500",
+      bulletColor: "before:bg-red-500"
+    },
+    {
+      iconName: developer,
+      roleName: "Developer",
+      userCount: 75,
+      listItems: [
+        "Develop New Features",
+        "Bug Fixes",
+        "Code Reviews",
+        "Unit Testing"
+      ],
+      borderColor: "border-t-green-500",
+      bulletColor: "before:bg-green-500"
+    },
+    {
+      iconName: support,
+      roleName: "Support ",
+      userCount: 45,
+      listItems: [
+        "Handle Customer Queries",
+        "Issue Resolution",
+        "Product Assistance",
+        "Customer Follow-ups"
+      ],
+      borderColor: "border-t-blue-500",
+      bulletColor: "before:bg-blue-500"
+    },
+    {
+      iconName: dataentry,
+      roleName: "Data Entry",
+      userCount: 30,
+      listItems: [
+        "Data Entry Tasks",
+        "Data Verification",
+        "Record Management",
+        "Reporting Discrepancies"
+      ],
+      borderColor: "border-t-yellow-500",
+      bulletColor: "before:bg-yellow-500"
+    }
+  ];
+  
+  export default roles;
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,23 +1,23 @@
 'use client';
 import RoleCard from '@/components/card/rolecard';
-import admin from '../../../public/images/RoleImages/admin.svg';
+import list from '@/components/card/rolecardlist';
 
 export default function Test() {
   return (
-    <div>
-      <RoleCard
-        iconName={admin}
-        roleName="Administrator"
-        userCount={10}
-        listItems={[
-          'Full User Management',
-          'Access Control Lists',
-          'Audit Logs',
-          'System Settings',
-        ]}
-        borderColor="border-t-red-500"
-        bulletColor="before:bg-red-500"  // Specify the color for bullet points
-      />
+    <div className="flex flex-wrap">
+      {list.map((role, index) => (
+        <div className="p-5">
+          <RoleCard
+            key={index}
+            iconName={role.iconName}
+            roleName={role.roleName}
+            userCount={role.userCount}
+            listItems={role.listItems}
+            borderColor={role.borderColor}
+            bulletColor={role.bulletColor}
+          />
+        </div>
+      ))}
     </div>
   );
 }
