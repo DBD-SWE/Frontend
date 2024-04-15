9961cd0aeac2d2e08b1f4ad3a3d01acdebf7f59d
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-15 15:39:15 +0300

    add: pageTitle component

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,8 +1,9 @@
+import { PageTitle } from "@/components/text";
 
 export default function Users() {
   return (
-    <div className="flex items-center justify-center">
-     <h1>Users</h1>
+    <div className="flex flex-col">
+     <PageTitle>User Management</PageTitle>
     </div>
   );
 }
