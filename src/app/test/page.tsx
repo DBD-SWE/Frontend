f008af3f73d22e24e3793b17c93f39cd4bdaaf73
Peter Chahid <peterdaaboul@outlook.com>
2024-04-15 18:23:36 +0300

    made the card dynamic to take props

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,23 @@
 'use client';
-import RoleCard from "@/components/card/rolecard";
-export default function test(){
-    return(
-        <div>
-            <RoleCard/>
-        </div>
-       
-       
-    )
+import RoleCard from '@/components/card/rolecard';
+import admin from '../../../public/images/RoleImages/admin.svg';
+
+export default function Test() {
+  return (
+    <div>
+      <RoleCard
+        iconName={admin}
+        roleName="Administrator"
+        userCount={10}
+        listItems={[
+          'Full User Management',
+          'Access Control Lists',
+          'Audit Logs',
+          'System Settings',
+        ]}
+        borderColor="border-t-red-500"
+        bulletColor="before:bg-red-500"  // Specify the color for bullet points
+      />
+    </div>
+  );
 }
