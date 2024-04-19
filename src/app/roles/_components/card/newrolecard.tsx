d8e162f71aecb81582d6c00b911ad1abc4d6eb62
Peter Chahid <peterdaaboul@outlook.com>
2024-04-19 19:15:08 +0300

    test

--- src/app/roles/_components/card/newrolecard.tsx
+++ src/app/roles/_components/card/newrolecard.tsx
@@ -1,17 +1,9 @@
 import Image from 'next/image';
 import { Button } from '@nextui-org/react';
 
-const AddIcon = (
-  <Image
-    width={19}
-    height={17}
-    src={'/images/roles/plus.svg'}
-    alt="role icon"
-  />
-);
 export default function NewRoleCard() {
   return (
-    <div className="flex h-72 w-full flex-col justify-between rounded-lg border-2 border-b-transparent border-l-transparent border-r-transparent border-t-black p-4 shadow-sm">
+    <div className=" flex h-72 w-full flex-col justify-between rounded-lg border-2 border-t-black p-4">
       <div>
         <div className="mb-1 flex items-center justify-between">
           <h1 className="text-lg font-bold">Add New Role</h1>
@@ -25,7 +17,7 @@ export default function NewRoleCard() {
         <p className="pr-10 text-xs font-normal text-zinc-500">
           Create a new role and assign the respective permissions to it
         </p>
-        <div className="flex items-center justify-center pt-4">
+        <div className="flex items-center justify-center pt-7">
           <Image
             width={120}
             height={90}
@@ -38,7 +30,7 @@ export default function NewRoleCard() {
       </div>
       <div className="flex justify-center">
         <Button
-          endContent={AddIcon}
+          endContent={'/images/roles/plus.svg'}
           color="primary"
           className="h-8 w-40 rounded text-sm"
         >
