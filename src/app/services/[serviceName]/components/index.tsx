fc2260dafd0e1323a40df500417eb7a8c37144df
Jean-Paul <jean.paul.bassil@outlook.com>
2024-04-21 16:45:10 +0300

    removed: search filter and select

--- src/app/services/[serviceName]/components/index.tsx
+++ src/app/services/[serviceName]/components/index.tsx
@@ -1,4 +1,5 @@
 import Title from './Title';
 import SearchInput from './Search';
+import SelectDropDown from './SelectDropDown';
 
-export { Title, SearchInput };
+export { Title, SearchInput, SelectDropDown };
--- src/app/services/[serviceName]/page.tsx
+++ src/app/services/[serviceName]/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Title, SearchInput } from './components';
+import { Title, SearchInput, SelectDropDown } from './components';
 import { PathnameProvider } from '@/lib/context/PathNameProvider';
+import Image from 'next/image';
 
 const page = () => {
+  // const options = ['Hello 1', 'Hello 2', 'Hello 3 ', 'Hello 4']
   return (
     <PathnameProvider>
       <div className="flex flex-col gap-20 sm:px-8 sm:py-4">
@@ -13,9 +15,7 @@ const page = () => {
           </button>
         </header>
         <main className="w-full rounded-md bg-[#f4f4f5] px-[1px] py-[1px] pt-6">
-          <div className="w-full bg-white px-4 py-2 sm:px-8 sm:py-4">
-            <SearchInput />
-          </div>
+          
         </main>
       </div>
     </PathnameProvider>
