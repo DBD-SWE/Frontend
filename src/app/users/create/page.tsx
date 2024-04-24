6710efdef48858b882b2a21ac3b8a047e0a89a94
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-24 18:02:17 +0300

    finish personal section

--- src/app/users/create/page.tsx
+++ src/app/users/create/page.tsx
@@ -14,16 +14,16 @@ export default function CreateUser() {
       {/* Body Section */}
       <div className="my-12 w-full">
         {/* Personal Information */}
-        <div className="flex items-center justify-start border-b-[1px] border-t-[1px] border-b-[#F4F4F5] border-l-transparent border-r-transparent border-t-[#F4F4F5] px-4 py-14">
+        <div className="flex items-center justify-between border-b-[1px] border-t-[1px] border-b-[#F4F4F5] border-l-transparent border-r-transparent border-t-[#F4F4F5] px-4 py-14 max-md:flex-col max-md:items-start max-md:px-2 max-md:py-5">
           {/* Left Text */}
-          <div className="flex w-max flex-col">
+          <div className="flex flex-col">
             <h3>Personal Information</h3>
-            <p className="mt-0.5 w-max text-sm text-zinc-500">
+            <p className="mt-0.5 text-wrap text-sm text-zinc-500">
               Fill in basic information to establish the profile.
             </p>
           </div>
           {/* Right Inputs */}
-          <div className="ml-32 grid w-[100%] max-w-[700px] grid-cols-2 grid-rows-2 gap-x-5 gap-y-4 max-lg:ml-6 max-lg:grid-cols-1 max-md:grid-rows-4">
+          <div className="ml-0 grid w-[100%] max-w-[700px] flex-1 grid-cols-1 grid-rows-4 gap-x-5 gap-y-4 max-md:mt-7 md:ml-6 lg:ml-32 lg:grid-cols-2 lg:grid-rows-2">
             <Input type="text" label="First Name" />
             <Input type="text" label="Last Name" />
             <Input type="email" label="Email" />
