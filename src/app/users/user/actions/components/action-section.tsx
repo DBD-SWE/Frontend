62a1683b1d0c73764b3ef837fee90fabcdee1383
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-26 14:18:04 +0300

    styled action section

--- src/app/users/user/actions/components/action-section.tsx
+++ src/app/users/user/actions/components/action-section.tsx
@@ -1,10 +1,26 @@
-const ActionSection = () => {
-    return (
-			<div className="">
-
-			</div>
-		)
+//      ={`flex ${start && !center ? 'items-start' : 'items-center'} ${start ? 'max-md:flex-col max-md:items-center' : 'items-center'} justify-start   max-md:flex-col max-md:items-start max-md:px-2 max-md:py-6 ${last && 'border-b-[1px] border-b-[#F4F4F5]'}`}
+interface ActionSectionProps {
+  title: string;
+  description: string;
+  button: React.ReactNode;
+  last?: boolean; // bottom border for last element
 }
 
+const ActionSection = (props: ActionSectionProps) => {
+  const { title, description, button, last } = props;
+  return (
+    <div className="border-t-[1px] border-l-transparent border-r-transparent border-t-[#F4F4F5] px-4 py-7">
+      {/* Left Part */}
+      <div className="flex flex-col items-stretch">
+        <h3>{title}</h3>
+        <p className="mt-0.5 w-full text-wrap text-sm text-zinc-500">
+          {description}
+        </p>
+      </div>
+      {/* Right Part */}
+      <div className="mt-6 flex justify-end">{button}</div>
+    </div>
+  );
+};
 
 export default ActionSection;
--- src/app/users/user/actions/page.tsx
+++ src/app/users/user/actions/page.tsx
@@ -1,8 +1,37 @@
+import { Button } from '@nextui-org/react';
+import { ActionSection } from './components';
+import Image from 'next/image';
+
 export default function Actions() {
   return (
     <div className="flex w-full flex-col pb-10">
       {/* Body Section */}
-      <h1>Actions</h1>
+      <ActionSection
+        title="Edit User"
+        description="Adjust user personal information or software related information"
+        button={
+          <ButtonWithIcon text="Edit User" src="/images/general/edit-2.png" />
+        }
+      />
     </div>
   );
 }
+
+const ButtonWithIcon = ({ text, src }: { text: string; src: string }) => {
+  return (
+    <Button
+      endContent={
+        <Image
+          className="ml-2 object-contain"
+          width={15}
+          height={15}
+          src={src}
+          alt={text}
+        />
+      }
+      className="flex h-10 items-center rounded border border-black bg-white px-10 text-sm"
+    >
+      {text}
+    </Button>
+  );
+};
