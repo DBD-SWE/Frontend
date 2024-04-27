0eabfa4b49571f14bfd50857cc81092848692696
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-27 18:01:51 +0300

    add: header section

--- src/app/view-roles/page.tsx
+++ src/app/view-roles/page.tsx
@@ -1,15 +1,23 @@
-'use client';
 import DeleteCard from './components/card/deletecard';
 import ViewCard from './components/card/viewcard';
 import roles from '@/roles/components/card/rolecardlist'; // Make sure the import path is correct
 import ViewTable from './components/table/viewtable';
 import mockData from './components/table/tablelist';
+import { PageTitle, SubTitle } from '@/components/text';
 
 export default function ViewRoles() {
   const firstRole = roles[0]; // Get the first element from the roles array
   return (
-    <>
-      <div>
+    <div>
+      {/* Header Section */}
+      <div className="flex flex-col">
+        <PageTitle>Roles & Permissions</PageTitle>
+        <SubTitle
+          content={['Administrator Role', '○', 'Detailed Permissions ']}
+        />
+      </div>
+      {/* Body Section */}
+      <div className="py-10">
         <div className="pb-8">
           <ViewCard
             iconName={firstRole.iconName}
@@ -25,6 +33,6 @@ export default function ViewRoles() {
           <DeleteCard roleName={firstRole.roleName} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
