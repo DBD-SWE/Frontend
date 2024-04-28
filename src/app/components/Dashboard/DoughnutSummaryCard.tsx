456ec7151ad0f2412f9ee13c2d31cceec2b68a9a
Tony Kosseify <tonykosseify123@gmail.com>
2024-04-28 22:09:33 +0300

    styling: total services component

--- src/app/components/Dashboard/DoughnutSummaryCard.tsx
+++ src/app/components/Dashboard/DoughnutSummaryCard.tsx
@@ -21,12 +21,12 @@ const PieSummaryCard = ({
   colors,
 }: Props) => {
   return (
-    <div className="flex w-full sm:justify-between max-sm:gap-4 rounded-md border border-b-black max-sm:px-4 py-8 pb-10 shadow-sm flex-col max-sm:py-4 px-4 xl:pb-0">
+    <div className="flex w-full flex-col rounded-t border-b-2 border-b-black px-4 py-8 pb-10 shadow-sm max-sm:gap-4 max-sm:px-4 max-sm:py-4 sm:justify-between xl:pb-0">
       <div className="flex w-full flex-col">
         <div className="w-full">
           <p className="text-xl font-semibold max-sm:text-lg">Total {title}</p>
           <p className="mt-1 text-4xl font-bold max-sm:mt-0 max-sm:text-3xl">
-            {total}
+            +{total}
           </p>
           <p className="mt-3 text-xs text-zinc-500 max-sm:mt-0">
             Services distribution on districts <br />
@@ -34,11 +34,11 @@ const PieSummaryCard = ({
           </p>
         </div>
       </div>
-      <div className='flex justify-between'>
-        <div className="max-sm:self-center self-end w-1/2 lg:-translate-y-[60px]">
+      <div className="flex justify-between">
+        <div className="w-1/2 self-end max-sm:self-center lg:-translate-y-[60px]">
           <ChartLabelsList colors={colors} labels={labels} />
         </div>
-        <div className="h-36 w-36 self-center sm:h-60 sm:w-60 md:self-end max-sm:pb-2 xl:-translate-y-12">
+        <div className="h-36 w-36 self-center max-sm:pb-2 sm:h-60 sm:w-60 md:self-end xl:-translate-y-12">
           <DoughnutChart labels={labels} stats={stats} colors={colors} />
         </div>
       </div>
