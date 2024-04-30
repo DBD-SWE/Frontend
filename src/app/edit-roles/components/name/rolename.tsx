3519ff12457a48c438d3ed277e1cebfede145f5a
Peter Chahid <peterdaaboul@outlook.com>
2024-04-30 18:12:18 +0300

    made the page responsive

--- src/app/edit-roles/components/name/rolename.tsx
+++ src/app/edit-roles/components/name/rolename.tsx
@@ -14,7 +14,7 @@ export default function RoleName({ defaultRoleName }: RoleNameProps) {
           Role Name
         </h1>
         <Input
-          className="ml-4 w-[95%] pt-2 sm:ml-6 sm:w-auto sm:max-w-md md:ml-8 md:max-w-lg"
+          className="ml-2 w-[95%] pt-2 sm:ml-2 sm:w-auto sm:max-w-md md:ml-2 md:max-w-lg"
           defaultValue={defaultRoleName} // Use the prop for the default value
           input="text-zinc-500"
         />
