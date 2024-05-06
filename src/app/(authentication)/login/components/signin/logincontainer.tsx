4504e2f22dd61e004f961b386f6e508cc23908ce
Peter Chahid <peterdaaboul@outlook.com>
2024-05-06 20:23:00 +0300

    finalized the login card

--- src/app/(authentication)/login/components/signin/logincontainer.tsx
+++ src/app/(authentication)/login/components/signin/logincontainer.tsx
@@ -2,6 +2,8 @@
 import { Input } from '@/(general)/components/input';
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
+import { Button } from '@nextui-org/react';
 export default function LoginContainer() {
   const Mail = (
     <div className="flex flex-row items-center">
@@ -40,46 +42,69 @@ export default function LoginContainer() {
 
   const toggleVisibility = () => setIsVisible(!isVisible);
   return (
-    <div className="h-4/5 w-2/5 rounded-lg px-14 shadow">
+    <div className="flex h-[700px] w-2/5 flex-col justify-between rounded-lg px-14 shadow">
       {/* Heading  */}
-      <div className="w-full text-center">
-        <h1 className="pb-2 pt-12 text-[15px] font-bold">Sign In</h1>
-        <p className="text-[10px] font-light text-zinc-500">
-          Enter your credentials to access your account.
-        </p>
-        <div className="mt-8 h-[1px] bg-zinc-300" />
-      </div>
-      {/* Inputs */}
-      <div className="mt-10 w-full">
-        <div className="">
-          <Input
-            className="w-full self-center"
-            label="Email Address"
-            labelPlacement="outside"
-            placeholder=" "
-            variant="bordered"
-            classNames={{ inputWrapper: 'bg-white' }}
-            startContent={Mail}
-          />
-
-          <Input
-            className="w-full self-center"
-            label="Password"
-            labelPlacement="outside"
-            placeholder=" "
-            variant="bordered"
-            classNames={{ inputWrapper: 'bg-white' }}
-            startContent={
-              <button
-                className="focus:outline-none"
-                type="button"
-                onClick={toggleVisibility}
+      <div>
+        <div className="w-full text-center">
+          <h1 className="pb-2 pt-12 text-[15px] font-bold">Sign In</h1>
+          <p className="text-[10px] font-light text-zinc-500">
+            Enter your credentials to access your account.
+          </p>
+          <div className="mt-8 h-[1px] bg-zinc-300" />
+        </div>
+        {/* Inputs */}
+        <div className="mt-10 w-full">
+          <div className="pb-6 pt-12">
+            <Input
+              className="w-full self-center"
+              label="Email Address"
+              labelPlacement="outside"
+              placeholder=" "
+              variant="bordered"
+              classNames={{ inputWrapper: 'bg-white' }}
+              startContent={Mail}
+            />
+          </div>
+          <div>
+            <Input
+              className="w-full self-center "
+              label="Password"
+              labelPlacement="outside"
+              placeholder=" "
+              variant="bordered"
+              classNames={{ inputWrapper: 'bg-white' }}
+              startContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={toggleVisibility}
+                >
+                  {isVisible ? Hide : View}
+                </button>
+              }
+              type={isVisible ? 'text' : 'password'}
+            />
+            {/* Forgot password link */}
+            <div className="text-right">
+              <Link
+                href="/forgot-password"
+                className="text-xs font-medium italic text-zinc-500 hover:text-blue-800"
               >
-                {isVisible ? Hide : View}
-              </button>
-            }
-            type={isVisible ? 'text' : 'password'}
-          />
+                Forgot password?
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        {/* Button  */}
+        <div className="flex justify-center pb-20">
+          <Button
+            color="primary"
+            className="flex-end h-10 w-full rounded px-11 text-sm"
+          >
+            Sign in
+          </Button>
         </div>
       </div>
     </div>
